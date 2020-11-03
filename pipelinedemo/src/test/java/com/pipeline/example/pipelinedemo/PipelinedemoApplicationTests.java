7bc5d62b5ddef0942a2d21b2e427238d59c42492
kumapro <prem.mishra456@gmail.com>
2020-11-03 15:28:24 +0530

    jenkins freestyle demo

---
+++ pipelinedemo/src/test/java/com/pipeline/example/pipelinedemo/PipelinedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.example.pipelinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelinedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
